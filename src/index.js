d1a40b28d1d50485eef82d1b471e989d78006050
jdltechworks <jose@bywave.com.au>
2017-01-20 15:43:01 +0800

    Form handling

--- src/index.js
+++ src/index.js
@@ -2,72 +2,43 @@ import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import _ from 'lodash';
 
-const Foo = () => {
-  return(
-    <div>The function</div>
-  );
-}
-
-let people = [
-  { name: 'ellen sabaan', age: 100 },
-  { name: 'LEO', age: 500 },
-  { name: 'rose', age: 11},
-  { name: 'mae', age: 1 },
-  { name: 'gerard', age: 11},
-  { name: 'isiel', age: 1 }
-];
-
-class Hello extends Component  {
-  _onClick(e) {
-    console.log(this);
-    console.log('clicked');
-    alert('clicked');
-  }
-  render() {
-    let { props } = this;
-    let { isActive } = props;
-    let grouped = _.chunk(people, 3);
-    return(
-      <div>
-        {_.toString(isActive)}
-      </div>
-    );
-  }
-}
-
-class StateSample extends Component {
+class FormSample extends Component {
   state = {
-    isActive: false
+    message: '',
+    data: []
   };
+  componentDidMount() {
+    fetch('http://matrix.f45.info/v1/franchisees', {
+      method: 'GET'
+    }).then((res) => {
+      return res.json();
+    }).then((json) => {
+      this.setState({ data: json.data });
+    }).catch(err => console.log(err));
+  }
+  handleSubmit() {
+
+  }
   render() {
-    let { state } = this;
-    let { isActive } = state;
-    let grouped = _.chunk(people, 3);
+    let { handleSubmit } = this;
+    console.log(this.state.data);
     return(
-      <div>
-        <a href="#" onClick={(e) => {
-          this.setState({ isActive: !this.state.isActive });
-        }}>toggle</a>
-        { isActive ? <div>
-          <Hello isActive={isActive} />
-          <h2>Mga pips</h2>
-          {_.map(grouped, (pips, key) => {
-            return <ul key={key}>
-              {_.map(pips, (person, index) => {
-                return <li key={index}>
-                  <span>Name: {person.name}</span>
-                  &nbsp;
-                  <span>Age: {person.age}</span></li>
-              })}
-            </ul>
-        })}
-        </div> : null }
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit.bind(this)}>
+          <div className="form-group">
+            <label className="form-label">First name</label>
+            <input ref="firstName" className="form-control" type="text" />
+            <label className="form-label">Last name</label>
+            <input ref="lastName" className="form-control" type="text" />
+            <button className="btn btn-success btn-block">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-render(<StateSample />, document.getElementById('app'));
+render(<FormSample />, document.getElementById('app'));
 
 if(process.env.NODE_ENV === 'development') {
   module.hot.accept();
